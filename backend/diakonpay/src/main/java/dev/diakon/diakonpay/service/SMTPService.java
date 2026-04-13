979b6b4270b266d590b753e4091b365554940b81
IT_diakon <daniel.nurgaliev2005@gmail.com>
2026-04-13 07:20:01 +0300

    formated code in SMTPService

--- backend/diakonpay/src/main/java/dev/diakon/diakonpay/service/SMTPService.java
+++ backend/diakonpay/src/main/java/dev/diakon/diakonpay/service/SMTPService.java
@@ -32,23 +32,34 @@ public class SMTPService {
 
     public void sendMessageAboutRegistry(String userName, String email) {
         String subject = "Добро пожаловать в DiakonPay!";
-        String text = "Здравствуйте, " + userName + "!\n\n" +
-                "Мы рады приветствовать вас в приложении DiakonPay.\n" +
-                "Ваш аккаунт успешно зарегистрирован и готов к работе.\n\n" +
-                "Следите за обновлениями на нашем GitHub:\n\n" +
-                "https://github.com/Dane4ka-in-IT/diakon-pay\n\n" +
-                "\t\t\t\t\t\t\tC уважением, команда diakon-team!";
+        String text = String.format("""
+                Здравствуйте, %s!
+
+                Мы рады приветствовать вас в приложении DiakonPay.
+                Ваш аккаунт успешно зарегистрирован и готов к работе.
+
+                Следите за обновлениями на нашем GitHub:
+                https://github.com/Dane4ka-in-IT/diakon-pay
+
+                \t\t\t\t\t\t\tC уважением, команда diakon-team!""",
+                userName);
+
         sendMail(email, subject, text);
     }
 
     public int sendMessageAboutChangedEmail(String userName, String oldEmail, String newEmail) {
         int randomNumber = new Random().nextInt(9000) + 1000;
         String subject = "Cмена почты в DiakonPay!";
-        String text = "Здравствуйте, " + userName + "!\n\n" +
-                "Вы подали запрос на смену почты. Если это были не вы - срочно смените пароль." +
-                "Ожидаемая новая почта: " + newEmail + "." +
-                "Для подтверждения введите код: " + randomNumber + ".\n\n" +
-                "\t\t\t\t\t\t\tC уважением, команда diakon-team!";
+        String text = String.format("""
+                Здравствуйте, %s!
+
+                Вы подали запрос на смену почты. Если это были не вы - срочно смените пароль.
+                Ожидаемая новая почта: %s.
+                Для подтверждения введите код: %d.
+
+                \t\t\t\t\t\t\tC уважением, команда diakon-team!""",
+                userName, newEmail, randomNumber);
+
         sendMail(oldEmail, subject, text);
         return randomNumber;
     }
@@ -56,10 +67,15 @@ public class SMTPService {
     public int sendMessageAboutRecoveryPass(String userName, String email) {
         int randomNumber = new Random().nextInt(9000) + 1000;
         String subject = "Смена пароля в DiakonPay!";
-        String text = "Здравствуйте, " + userName + "!\n\n" +
-                "Вы подали запрос на смену пароля. Если это были не вы - проигнорируйте это сообщение." +
-                "Ваш временный код: " + randomNumber + ".\n\n"  +
-                "\t\t\t\t\t\t\tC уважением, команда diakon-team!";
+        String text = String.format("""
+                Здравствуйте, %s!
+
+                Вы подали запрос на смену пароля. Если это были не вы - проигнорируйте это сообщение.
+                Ваш временный код: %d.
+
+                \t\t\t\t\t\t\tC уважением, команда diakon-team!""",
+                userName, randomNumber);
+
         sendMail(email, subject, text);
         return randomNumber;
     }
@@ -67,14 +83,16 @@ public class SMTPService {
     public void sendMonthlyReport(String userName, String email, String month,
                                   double totalSpent, int transactionsCount) {
         String subject = "Ваш отчет в DiakonPay за " + month;
-
-        String text = String.format(
-                "Здравствуйте, %s!\n\n" +
-                        "Ваш краткий отчет за %s готов:\n" +
-                        "— Всего потрачено: %.2f руб.\n" +
-                        "— Количество операций: %d\n\n" +
-                        "Полную детализацию вы можете увидеть в приложении.\n\n" +
-                        "\t\t\t\t\t\t\tС уважением, команда diakon-team!",
+        String text = String.format("""
+                Здравствуйте, %s!
+                
+                Ваш краткий отчет за %s готов:
+                - Всего потрачено: %.2f руб.
+                - Количество операций: %d
+                
+                Полную детализацию вы можете увидеть в приложении.
+                
+                \t\t\t\t\t\t\tС уважением, команда diakon-team!""",
                 userName, month, totalSpent, transactionsCount
         );
 
